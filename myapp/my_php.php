c63cfb789d3138c6b2a916e169438ce85b37622e
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:10:54 +0200

    new changes

---
+++ myapp/my_php.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles_1.css">
+    <script src="My_Js.js"></script>
+    <title>bruce</title>
+    <link rel="styles.css" />
+</head>
+<body>
+    <p id="bruce" style="border: 1px solid green ">Bruce is fantastic</p>
+    <button type="button" >click me</button>
+    <?php
+    $name ="Bruce";
+    echo "Hello name"?>
+</body>
+</html>
