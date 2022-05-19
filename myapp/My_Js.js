271c6db3ddcc8416ea27964cfd0749f4afff1a62
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-05-19 14:10:49 +0200

    new changes

---
+++ myapp/My_Js.js
@@ -0,0 +1,23 @@
+var c=0;
+var me="bruce"
+// my output function is console.log()
+my_list=["bruce","john","yves"]
+function my_function(name){
+ console.log(name+" is a good boy")
+}
+function square(number){
+    return number*number
+}
+square_of_5=square(5)
+console.log(square_of_5)
+var car={colour:"red",type:"fiat",weight:"678 kg"}
+console.log(car["colour"])
+//to call my function 
+my_function(me)
+console.log(c);
+console.log(my_list.join(" "))
+
+function please(){
+    document.getElementById("bruce").innerHTML="start php"
+}
+v
